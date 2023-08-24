8575c9dd7ce03c46b6dcfe4cc61982b1296315ba
iman <dr.iodeveloper@gmail.com>
2023-08-24 12:22:16 +0330

    update first navbar

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,17 @@
 @section('home')
 
 
+<div>
+    <img src="/shop_storage/carosual1.jpg" alt="" width="100%">
+    <img src="/shop_storage/carosual2.jpg" alt="" width="100%">
+    <img src="/shop_storage/carosual3.jpg" alt="" width="100%">
+</div>
 
-
-
-
+f;khnagkfgk;lag;la
+<br>
+    algpam
+<br>
+    pakg[k[g
 
 
 
