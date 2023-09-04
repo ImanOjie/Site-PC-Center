69374052755021d4363fcc7d0df3b43e44791689
iman <dr.iodeveloper@gmail.com>
2023-09-04 12:28:59 +0330

    update login register form

--- resources/views/pages/login_register.blade.php
+++ resources/views/pages/login_register.blade.php
@@ -4,34 +4,33 @@
 
     <div class="container" id="container">
         <div class="form-container sign-up-container">
-            <form action="#">
-                <h1>Sign Up</h1>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button>Sign Up</button>
+            <form action="#" method="post">
+                <h1>ثبت نام</h1>
+                <input type="text" placeholder="نام" />
+                <input type="email" placeholder="ایمیل" />
+                <input type="password" placeholder="رمز ورود" />
+                <button>عضویت</button>
             </form>
         </div>
         <div class="form-container sign-in-container">
-            <form action="#">
-                <h1>Login</h1>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <a href="#">Forgot your password?</a>
-                <button>Login</button>
+            <form action="#" method="post">
+                <h1>ورود</h1>
+                <input type="text" placeholder="نام" />
+                <input type="password" placeholder="رمز ورود" />
+                <button>ورود</button>
             </form>
         </div>
         <div class="overlay-container">
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
-                    <h1>Hey Friend</h1>
-                    <p>To keep connected with us please login</p>
-                    <button class="ghost" id="signIn">Sign In</button>
+                    <h1>کاربر گرامی</h1>
+                    <h3>اگر قبلا ثبت نام کردی از اینجا وارد شو</h3>
+                    <button class="ghost" id="signIn">ورود</button>
                 </div>
                 <div class="overlay-panel overlay-right">
-                    <h1>If you have not sign up yet </h1>
-                    <p>Enter your personal details and be one of us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
+                    <h1> کاربر گرامی </h1>
+                    <h3>اگر ثبت نام نکردی اطلاعاتت رو اینجا وارد کن و عضوی از ما شو </h3>
+                    <button class="ghost" id="signUp">ثبت نام</button>
                 </div>
             </div>
         </div>
