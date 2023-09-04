43c379b39833a1c4b7ef458ea14bcc0b3a2a326b
iman <dr.iodeveloper@gmail.com>
2023-09-04 17:04:14 +0330

    update login codes

--- resources/views/pages/login_register.blade.php
+++ resources/views/pages/login_register.blade.php
@@ -4,19 +4,40 @@
 
     <div class="container" id="container">
         <div class="form-container sign-up-container">
+            {{--@if(session()->has('save_ok_shod'))
+                    {{session()->get('save_ok_shod')}}
+                @endif--}}
             <form action="#" method="post">
+                @csrf
                 <h1>ثبت نام</h1>
-                <input type="text" placeholder="نام" />
-                <input type="email" placeholder="ایمیل" />
-                <input type="password" placeholder="رمز ورود" />
+
+                @if($errors->has('name'))
+                    {!! $errors->first('name') !!}
+                @endif
+                <input type="text" name="name" placeholder="نام" />
+
+                @if($errors->has('email'))
+                    {!! $errors->first('email') !!}
+                @endif
+                <input type="email" name="email" placeholder="ایمیل" />
+
+                @if($errors->has('password'))
+                    {!! $errors->first('password') !!}
+                @endif
+                <input type="password" name="password" placeholder="رمز ورود" />
+
                 <button>عضویت</button>
             </form>
         </div>
         <div class="form-container sign-in-container">
             <form action="#" method="post">
+                @csrf
                 <h1>ورود</h1>
+
                 <input type="text" placeholder="نام" />
+
                 <input type="password" placeholder="رمز ورود" />
+
                 <button>ورود</button>
             </form>
         </div>
@@ -24,7 +45,7 @@
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
                     <h1>کاربر گرامی</h1>
-                    <h3>اگر قبلا ثبت نام کردی از اینجا وارد شو</h3>
+                    <h3>اگر ثبت نام کردی از اینجا وارد شو</h3>
                     <button class="ghost" id="signIn">ورود</button>
                 </div>
                 <div class="overlay-panel overlay-right">
