b69c32d94165e3d4ade4c0cfc76371a9dad0a5c5
iman <dr.iodeveloper@gmail.com>
2023-11-28 12:20:56 +0330

    update product page

--- resources/views/pages/menu/products_page.blade.php
+++ resources/views/pages/menu/products_page.blade.php
@@ -2,10 +2,19 @@
 
 @section('home')
 
-    <div class="text-center">
+    {{--<div class="row page-row">
         <img src="{{$item->image }}" width="250px"/>
         <p class="text-body">{{$item->category}} {{$item->brand}} مدل {{$item->fullname}}</p>
+    </div>--}}
+    <div class="row row-product-image">
+        <div class="col-md-6 col-image">
+            <img src="{{$item->image }}"/>
+        </div>
+        <div class="col-md-6 col-image-info">
+
+        </div>
     </div>
+
 
 
 
