776e6b0b2113b3df1ba200671c11d061370bb931
iman <dr.iodeveloper@gmail.com>
2023-12-07 17:05:43 +0330

    update footer logo html css

--- resources/views/layouts/home_layout.blade.php
+++ resources/views/layouts/home_layout.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('/css/home.css')}}">
     <link rel="stylesheet" href="{{asset('/css/list-products.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/page-product.css)}}">
+    <link rel="stylesheet" href="{{asset('/css/page-product.css')}}">
     <link rel="icon" type="image/x-icon" href="{{asset('/shop_storage/pc-favicon.png')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
@@ -15,11 +15,12 @@
 <body onload="loading()">
 
 {{--header navbar 1--}}
-
 <header>
     <div class="wrapper">
         <div class="logo">
-            <a href="{{route('home')}}"><img src="{{asset('shop_storage/pc-logo.png')}}" alt="pccenter-logo" width="150px"></a>
+            <a href="{{route("home")}}">
+                <img src="{{asset("/shop_storage/pc-logo.png")}}" alt="pccenter-logo" width="150px">
+            </a>
         </div>
         <div class="navbar">
 
@@ -32,14 +33,13 @@
 
         </div>
         <div class="links-nav1">
-            <a href=""><button class="item_nav_link1">9903989716(+98)<img src="{{asset('shop_storage/icon-call.png')}}" alt="icon-call" style="height: 20px;padding: 0px 8px 0px 0px"></button></a>
-            <a href=""><button class="item_nav_link2">pc_center@gmail.com<img src="{{asset('shop_storage/icon-email.png')}}" alt="icon-email" style="height: 20px;padding: 0px 8px 0px 0px"></button></a>
+            <a href=""><button class="item_nav_link1">9903989716(+98)<img src="{{asset("shop_storage/icon-call.png")}}" alt="icon-call" style="height: 20px;padding: 0px 8px 0px 0px"></button></a>
+            <a href=""><button class="item_nav_link2">pc_center@gmail.com<img src="{{asset("shop_storage/icon-email.png")}}" alt="icon-email" style="height: 20px;padding: 0px 8px 0px 0px"></button></a>
         </div>
     </div>
 </header>
 
 {{--header navbar 2--}}
-
 <div class="wrapper2">
     <div class="menu-bar">
         <button class="menu-btn"><i class="fa fa-bars" aria-hidden="true"></i></button>
@@ -48,8 +48,8 @@
             <div class="menu-bar2">
                 <button class="items-hover">قطعات کامپیوتر و لوازم جانبی</button>
                 <div class="hover-drop2">
-                    <button class="items-hover2"><a href="{{route('motherboard')}}">مادربرد</a></button>
-                    <button class="items-hover2"><a href="{{route('graphic')}}">کارت گرافیک</a></button>
+                    <button class="items-hover2"><a href="{{route("motherboard")}}">مادربرد</a></button>
+                    <button class="items-hover2"><a href="{{route("graphic")}}">کارت گرافیک</a></button>
                     <button class="items-hover2"><a href="">پردازنده</a></button>
                     <button class="items-hover2"><a href="">حافظه رم</a></button>
                     <button class="items-hover2"><a href="">مانیتور</a></button>
@@ -86,19 +86,18 @@
 
         </div>
     </div>
-
     <div class="sidebar">
         <div class="close-nav">
             <button class="sign-in-mobile">
                 @auth()
                     <span><i class="fa fa-user-circle px-2" aria-hidden="true"></i>{{auth()->user()->name}}</span>
-                    <form action="{{route('logout')}}" method="post">
+                    <form action="{{route("logout")}}" method="post">
                         @csrf
                         <button class="sign-out">خروج از حساب</button>
                     </form>
                 @endauth
                 @guest()
-                    <a href="{{route('login_register')}}"><button>ورود / ثبت نام</button></a>
+                    <a href="{{route("login_register")}}"><button>ورود / ثبت نام</button></a>
                 @endguest
             </button>
             <button class="exit">×</button>
@@ -107,8 +106,8 @@
             <div class="dropdown">
                 <button class="drop-btn" onclick="drop1()">قطعات کامپیوتر و لوازم جانبی<i class="fa fa-angle-double-left"></i></button>
                 <div class="list-items" id="list-items1">
-                    <a href="{{route('motherboard')}}" class="deep">مادربرد</a>
-                    <a href="{{route('graphic')}}" class="deep">کارت گرافیک</a>
+                    <a href="{{route("motherboard")}}" class="deep">مادربرد</a>
+                    <a href="{{route("graphic")}}" class="deep">کارت گرافیک</a>
                     <a href="#" class="deep">پردازنده</a>
                     <a href="#" class="deep">حافظه رم</a>
                     <a href="#" class="deep">فن خنک کننده</a>
@@ -147,47 +146,43 @@
             </div>
         </div>
     </div>
-
     <div class="search">
         <form action="#">
             <input type="text" placeholder="کلمه مورد نظر را وارد کنید">
-            <button type="submit"><img src="/shop_storage/icon-search.png" alt="" width="25px"></button>
+            <button type="submit"><img src="{{asset("/shop_storage/icon-search.png")}}" alt="" width="25px"></button>
         </form>
     </div>
-
     <div class="sign-in">
         @auth()
             <span><i class="fa fa-user-circle px-2" aria-hidden="true"></i>{{auth()->user()->name}}</span>
 
-            <form action="{{route('logout')}}" method="post">
+            <form action="{{route("logout")}}" method="post">
                 @csrf
                 <button class="sign-out">خروج از حساب</button>
             </form>
         @endauth
 
         @guest()
-            <a href="{{route('login_register')}}"><button class="sign-in-btn">ورود / ثبت نام</button></a>
+            <a href="{{route("login_register")}}"><button class="sign-in-btn">ورود / ثبت نام</button></a>
         @endguest
     </div>
-
     <div class="basket">
         <button>
-            <img src="/shop_storage/icon-shopping.png" alt="" width="40px">
+            <img src="{{asset("/shop_storage/icon-shopping.png")}}" alt="" width="40px">
         </button>
     </div>
 </div>
 <div class="search2">
     <form action="#">
         <input type="text" name="search" placeholder="کلمه مورد نظر را وارد کنید">
-        <button type="submit"><img src="/shop_storage/icon-search.png" alt="" width="25px"></button>
+        <button type="submit"><img src="{{asset("/shop_storage/icon-search.png")}}" alt="" width="25px"></button>
     </form>
 </div>
 
-@yield('home')
-
-{{--footer navbar1--}}
+@yield("home")
 
 <footer>
+    {{--footer navbar1--}}
     <div class="nav-footer">
         <div class="text">
             <i class="fa fa-user-plus" aria-hidden="true"></i>
@@ -205,25 +200,19 @@
         </form>
     </div>
 
-
     <div class="footer">
 
-{{--footer logo--}}
-        <div class="row footer1">
-            <div class="col-3">
-                <a href="{{route('home')}}">
-                    <img src="{{asset('/shop_storage/pc-logo.png')}}" alt="pc-center-logo" width="150px"
-                         class="logo footer-logo">
-                </a>
-            </div>
-            <div class="col-9"></div>
+        {{--footer logo--}}
+
+    <div class="footer-logo">
+            <img src="{{asset('shop_storage/pc-logo.png')}}" alt="">
         </div>
 
-{{--footer call--}}
+        {{--footer call--}}
 
         <div class="row footer2">
             <div class="col-3">
-                <img src="{{asset('/shop_storage/guidance-icon.png')}}" alt="guidance-icon" width="40px">
+                <img src="{{asset("/shop_storage/guidance-icon.png")}}" alt="guidance-icon" width="40px">
                 <span>نیاز به راهنمایی دارید؟</span>
             </div>
             <div class="col-3 guidance-num">
