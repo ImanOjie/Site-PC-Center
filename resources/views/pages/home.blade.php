b4e3a7db2fbd43f1aa760d7d4cd567b7580119b2
iman <dr.iodeveloper@gmail.com>
2023-12-09 16:05:05 +0330

    update home contain

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,29 +2,22 @@
 
 @section('home')
 
-    {{--carousal--}}
+{{--carousal--}}
 
 
 
-    {{--card--}}
+{{--card--}}
+<div class="card-link">
+    <a href="{{route("motherboard")}}"><img src="{{asset('/shop_storage/card-link1.webp')}}" alt=""></a>
+    <a href="#"><img src="{{asset('/shop_storage/card-link2.webp')}}" alt=""></a>
+    <a href="{{route("graphic")}}"><img src="{{asset('/shop_storage/card-link3.webp')}}" alt=""></a>
+    <a href="#"><img src="{{asset('/shop_storage/card-link4.webp')}}" alt=""></a>
+</div>
 
-    <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10 card-link">
-            <a href="" class="card"><img src="{{asset('/shop_storage/card-link1.webp')}}" alt="" width="130%" style="border-radius: 20px"></a>
-            <a href="" class="card"><img src="{{asset('/shop_storage/card-link2.webp')}}" alt="" width="130%" style="border-radius: 20px"></a>
-            <a href="" class="card"><img src="{{asset('/shop_storage/card-link3.webp')}}" alt="" width="130%" style="border-radius: 20px"></a>
-            <a href="" class="card"><img src="{{asset('/shop_storage/card-link4.webp')}}" alt="" width="130%" style="border-radius: 20px"></a>
-        </div>
-        <div class="col-md-1"></div>
-    </div>
 
-    <div class="row big-card">
-        <div class="col-1"></div>
-        <div class="col-10">
-            <a href=""><img src="{{asset('/shop_storage/gif-card.gif')}}" alt="" width="100%"></a>
-        </div>
-        <div class="col-1"></div>
-    </div>
+<div class="big-card">
+    <img src="{{asset('/shop_storage/gif-card.gif')}}" alt="">
+</div>
+
 
 @endsection
